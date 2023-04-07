28630d8c457f7f25c71e629b4f2cdf0302827abf
Rohit Avadhani <rohit.avadhani@gmail.com>
2023-04-07 09:47:11 +0100

    modified:   dependencies/database_connector.py

--- dependencies/database_connector.py
+++ dependencies/database_connector.py
@@ -1,5 +1,5 @@
 import pyodbc
-connection_string = 'Driver={ODBC Driver 18 for SQL Server};Server=172.20.2.4\mssql;Database=AMPSWDCPAC;Uid=rohit.avadhani;Pwd=Avadro.!994;'
+connection_string = 'Driver={ODBC Driver 18 for SQL Server};Server=172.20.2.4\mssql;Database=AMPSWDCPAC;Uid=rohit.avadhani;Pwd=Avadro.!994;TrustServerCertificate=yes;'
 class DatabaseConnector:
     def __init__(self, connection_string):
         self.connection_string = connection_string
